cfa4b54100c5afc60a9c63d6f125454968b2aef6
Alexandre Moine <alexandre@moine.me>
2018-04-20 17:42:28 +0200

    Start the proof of search

---
+++ search.v
@@ -0,0 +1,74 @@
+Require Import Nat.
+Require Import Arith.EqNat.
+Require Import Bool.
+Require Import List.
+Require Import Logic.Classical_Prop.
+
+Fixpoint search (n : nat) (lst : list nat) := 
+  match lst with
+  | nil => false
+  | cons n' lst' => if (beq_nat n n') then true else search n lst'
+  end.
+
+Lemma searchEmpty (n : nat): search n nil = false.
+Proof.
+  simpl.
+  reflexivity.
+Qed.
+
+Lemma recSearch (n : nat) (n': nat) (lst : list nat): search n (n'::lst) = orb (beq_nat n n') (search n lst).
+Proof.
+  auto.
+Qed.
+
+Lemma isTrueEq (b:bool): Is_true b <-> b = true.
+Proof.
+  split.
+  - apply Is_true_eq_true.
+  - apply Is_true_eq_left.
+Qed.
+
+Theorem search_ok (n: nat) (lst: list nat): In n lst <-> Is_true (search n lst).
+Proof.
+  split.
+  - induction lst.
+    -- simpl.
+      intros proof_of_False.
+      case proof_of_False. 
+
+    -- intros ou.
+      case ou.
+
+      --- intros a_n.
+        rewrite a_n.
+        apply isTrueEq.
+        rewrite -> recSearch.
+        rewrite <- beq_nat_refl.
+        apply orb_true_l.
+
+     --- intros in_n.
+        rewrite -> recSearch.
+        apply IHlst in in_n.
+        apply isTrueEq.
+        apply isTrueEq in in_n.
+        apply orb_true_iff.
+        refine (or_intror _).
+        exact in_n.
+
+    - induction lst.
+      -- simpl.
+        intros false. exact false.
+
+      -- intros H.
+        case (classic (eq_nat a n)).
+        --- intros a_n.
+            simpl.
+            apply eq_nat_is_eq in a_n.
+            refine (or_introl _).
+            exact (a_n).
+        --- intros na_n.
+            simpl.
+            refine (or_intror _).
+
+      
+Qed.
