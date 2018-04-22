9cfb23580d0b3efb758cbbb274eec2a9a1cc0ada
Alexandre Moine <alexandre@moine.me>
2018-04-22 20:55:44 +0200

    Good search

--- search.v
+++ search.v
@@ -33,22 +33,12 @@ Proof.
   apply orb_prop_elim in H.
   case H.
     - intros n_a.
-    apply isTrueEq in n_a.
-    apply beq_nat_true in n_a.
     contradict n_a.
+    rewrite isTrueEq.
+    rewrite beq_nat_true_iff.
     exact (not_eq_sym H0).
     - intros ok. exact ok.
 Qed.
-
-(* Copied from Arith.PeanoNat, as my coq can find it *)
-Lemma eq_dec : forall n m : nat, {n = m} + {n <> m}.
-Proof.
-  induction n; destruct m.
-  - now left.
-  - now right.
-  - now right.
-  - destruct (IHn m); [left|right]; auto.
-Defined.
 
 Theorem search_ok (n: nat) (lst: list nat): In n lst <-> Is_true (search n lst).
 Proof.
@@ -61,11 +51,12 @@ Proof.
       case ou.
 
       --- intros a_n.
-        rewrite a_n.
         apply isTrueEq.
         rewrite -> recSearch.
-        rewrite <- beq_nat_refl.
-        apply orb_true_l.
+        rewrite orb_true_iff.
+        refine (or_introl _).
+        rewrite beq_nat_true_iff.
+        exact (eq_sym a_n).
 
      --- intros in_n.
         rewrite -> recSearch.
@@ -81,16 +72,18 @@ Proof.
       intros false. exact false.
 
     -- intros H.
-      case (eq_dec a n).
+      case (eq_nat_decide a n).
       --- intros a_n.
           simpl.
           refine (or_introl _).
-          exact (a_n).
+          apply eq_nat_is_eq.
+          exact a_n.
       --- intros na_n.
           simpl.
           refine (or_intror _).
           apply IHlst.
           apply inTail in H.
           exact H.
+          rewrite <- eq_nat_is_eq.
           exact na_n.
 Qed.
